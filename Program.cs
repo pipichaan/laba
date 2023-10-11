2381840439deb87ce03f3a572eaf4a381ac67d9a
pipichaan <147584671+pipichaan@users.noreply.github.com>
2023-10-11 08:57:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите количество недель");
+int week = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество месяцев");
+int month = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество лет");
+int year = Convert.ToInt32(Console.ReadLine());
+int day = week * 7 + month * 30 + year * 365;
+Console.WriteLine(day);
+
